a1653f050d4d17efbdc414d901973b89f0f2a40c
Uliana <u.trubach08@gmail.com>
2026-03-17 18:22:02 +0200

    updated homework task 1.3.1

--- hw_131/main_hw_131.py
+++ hw_131/main_hw_131.py
@@ -12,6 +12,8 @@ class Triangle:
         p = (self.a + self.b + self.c)/2
         ar = (p*(p-self.a)*(p-self.b)*(p-self.c))**0.5
         return ar
+    def __str__(self):
+        return f"Triangle = {self.a, self.b, self.c}, perimeter = {self.perimeter()}, area = {self.area()}\n"
 
 class Rectangle:
     def __init__ (self, a,b):
@@ -22,6 +24,8 @@ class Rectangle:
         return (self.a + self.b) * 2
     def area(self):
         return self.a * self.b
+    def __str__(self):
+        return f"Rectangle = {self.a, self.b}, perimeter = {self.perimeter()}, area = {self.area()}\n"
 
 class Trapeze:
     def __init__ (self, a, b, c, d):
@@ -37,6 +41,8 @@ class Trapeze:
         assert (p-self.c)*(p-self.d)*(p-self.b)*(p-self.b-self.d) > 0
         ar = ((self.a+self.b)/4*(self.a-self.b))*((p-self.c)*(p-self.d)*(p-self.b)*(p-self.b-self.d))**0.5
         return ar
+    def __str__(self):
+        return f"Trapeze = {self.a, self.b, self.c, self.d}, perimeter = {self.perimeter()}, area = {self.area()}\n"
 
 class Parallelogram:
     def __init__ (self, a, b, h):
@@ -48,23 +54,25 @@ class Parallelogram:
         return (self.a +self.b)*2
     def area(self):
         return (self.a + self.b)/2 * self.h
+    def __str__(self):
+        return f"Parallelogram = {self.a, self.b, self.h}, perimeter = {self.perimeter()}, area = {self.area()}\n"
 
 
 class Circle:
     def __init__ (self, r):
         self.r = int(r)
-    def length(self):
+    def perimeter(self):
         return 2*pi*self.r
     def area(self):
         return pi*self.r**2
+    def __str__(self):
+        return f"Circle = {self.r}, length = {self.perimeter()}, area = {self.area()}\n"
 
+    
 fs = ["input01.txt", "input02.txt", "input03.txt"]
-maximum_tr = None
-maximum_rec = None
-maximum_trap = None
-maximum_par = None
-maximum_c = None
+res = []
 for f in fs:
+    figures = []
     with open(f, "r") as l:
         lines = l.readlines()
         for ls in lines:
@@ -74,70 +82,67 @@ for f in fs:
                 a, b, c = map(int, word[1:])
                 try:
                     tr = Triangle(a, b, c)
-                    maximum_tr = [tr, tr.perimeter(), tr.area()]
-                    p_tr = tr.perimeter()
-                    tr_ar = tr.area()
-                    if p_tr > maximum_tr[1] and tr_ar > maximum_tr[2]:
-                        maximum_tr = [tr, tr.perimeter(), tr.area()]
+                    figures.append(tr)
                 except AssertionError:
                     pass
+
+                
             if word[0] == "Rectangle":
                 a, b = map(int, word[1:])
                 try:
-                    rect = Rectangle(a, b)
-                    maximum_rec = [rect, rect.perimeter(), rect.area()]
-                    p_rec = rect.perimeter()
-                    ar_rec = rect.area()
-                    if p_rec > maximum_rec[1] and ar_rec > maximum_rec[2]:
-                        maximum_rect = [rect, rect.perimeter(), rect.area()]
+                   rec = Rectangle(a, b)
+                   figures.append(rec)
                 except AssertionError:
                    pass
+
+                
             if word[0] == "Trapeze":
                 a, b, c, d = map(int, word[1:])
                 try:
                     trap = Trapeze(a, b, c, d)
-                    p_trap = trap.perimeter()
-                    trap_ar = trap.area()
-                    maximum_trap = [trap, trap.perimeter(), trap.area()]
-                    if p_trap > maximum_tr[1] and trap_ar > maximum_tr[2]:
-                        maximum_tr = [trap, trap.perimeter(), trap.area()]
+                    figures.append(trap)
                 except AssertionError:
                     pass
+
+                
             if word[0] == "Parallelogram":
                 a, b, h = map(int, word[1:])
                 try:
                     par = Parallelogram(a, b, h)
-                    par_p = par.perimeter()
-                    par_ar = par.area()
-                    maximum_par = [par, par.perimeter(), par.area()]
-                    if par_p > maximum_par[1] and par_ar > maximum_par[2]:
-                        maximum_par = [par, par.perimeter(), par.area()]
+                    figures.append(par)
                 except AssertionError:
                     pass
+
+                
             if word[0] == "Circle":
-                for r in word[1]:
-                    try:
-                        circ = Circle(r)
-                        len_c = circ.length()
-                        ar_c = circ.area()
-                        maximum_c = [circ, circ.length(), circ.area()]
-                        if len_c > maximum_c[1] and ar_c > maximum_c[2]:
-                            maximum_c = [circ, circ.length(), circ.area()]
-                    except AssertionError:
-                        pass
-str_tr = f"Triangle: perimeter={maximum_tr[1]}, area={maximum_tr[2]}\n"
-str_rec = f"Rectangle: perimeter={maximum_rec[1]}, area={maximum_rec[2]}\n"
-str_trap = f"Trapeze: perimeter={maximum_tr[1]}, area={maximum_tr[2]}\n"
-str_par = f"Parallelogram: perimeter = {maximum_par[1]}, area = {maximum_par[2]}\n"
-str_c = f"Circle: length = {maximum_c[1]}, area = {maximum_c[2]}\n"
+                r = int(word[1])
+                try:
+                    circ = Circle(r)
+                    figures.append(circ)
+                except AssertionError:
+                    pass
+
+    a = 0
+    p = 0
+    figure = None
+    for i in figures:
+        try:
+            fig_a = i.area()
+            fig_p = i.perimeter()
+        except AssertionError:
+            continue
+        if fig_a>a and fig_p>p:
+            a = fig_a
+            p = fig_p
+            figure = i
+    res.append(figure)
+
+
+
 with open("output.txt", "w")as g:
-    g.write(str_tr)
-    g.write(str_rec)
-    g.write(str_trap)
-    g.write(str_par)
-    g.write(str_c)
-
-
+    g.write(str(res[0]))
+    g.write(str(res[1]))
+    g.write(str(res[2]))
 
 
 
